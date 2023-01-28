eec08a9076923de81e701488020a83334eb05474
akshat <akshat@akshats-MacBook-Pro.local>
2023-01-28 11:55:40 +0530

    constants with data type being better on memory allocation

--- variable_assigning/main.go
+++ variable_assigning/main.go
@@ -21,6 +21,18 @@ func main() {
 
 	fmt.Println(one, two) //1, 2
 
+	//  keep the data type in ming when assigning the const
+
+	const (
+		Pi               = 3.1456777
+		Avogardo float32 = 6.022e23
+	)
+
+	fmt.Println(Pi)
+	fmt.Println(reflect.TypeOf(Pi))
+	fmt.Println(Avogardo)
+	fmt.Println(reflect.TypeOf(Avogardo))
+
 	f, g := 10, 12 // multiple assigns
 
 	fmt.Println(f, g)
